c5c267ecb482412f02d936eb97638b2a5a8a36c0
anhad sidhu <anhadsidhu96@gmail.com>
2022-10-04 14:00:33 -0700

    game-setup

---
+++ README.md
@@ -0,0 +1,68 @@
+# ♠️ ♦️ Black Jack Game ♣️ ♥️
+
+## Unsolved Features:
+- The Ace is only worth 1 and not 11
+
+## Game Description:
+
+Blackjack is a very popular game not just in the casinos but also in the general public. Blackjack is also know as another game for kids called 21.
+
+So the objective of the game is to get to as close to 21 or on par with 21 with the initial cards you are dealt as well as ones you might call for. If you have a score over 21 then that player has lost. You can either hit and get another card or stay with the cards you have been given initially or have been given throughout the game.
+
+Each card number has a value, all the face cards such as Jack, Queen, and King are given a value of 10. All the values of the number cards correspond with that given number.
+
+The Ace card is special as it has duel values of 1 and 11. So a player can decide what value to give at a given time and even change that value throughout the game. So for example if you have an Ace and a Queen you would choose to make the Ace with a value of 11 so you get 21 and win. But lets say you have an Ace and a 7, then you call for a `Hit!` and now your third card is a 10. You would then want to change the value of the Ace card from an 11 to a 1. Since if you did not, you would be over 21 with those three cards and lose the game.
+
+## Game Play Breakdown
+
+- The game starts when the player clicks the `Let's Start` button.
+
+- After that the game has started and both you the player and the dealer are dealt two cards. Both your cards are faced up, the scores of both player and the dealer are given right below the text Player Score and Dealer Score. Also below the player score two buttons will appear since the game has started, which are `Hit!` and `Stay`. Between the players deck and the dealers deck there is also a `Restart` button as well.
+
+- So the game has started with the two card the players have been initially given. The player can decide to either `Hit!` or `Stay`, if they `Hit!` they will be given another card and can decide if they would like to `Hit!` again or `Stay`. If they decide to `Stay` then the dealer is dealt a card an based on that the player is once again given the choice to `Hit!` or `Stay`.
+
+- The game is over once either the dealer or player are over 21. A message will appear on the top of the screen right below the Blackjack header with "Player Wins!" or "Dealer Wins" based on how the game goes. The game will only result in a tie if both players get 21 and a message "It is a Tie" will come up on the for mentioned text area.
+
+- The player can then decide to `Restart` the game or just leave the site.
+
+
+## Game play Images
+
+Start of game:
+![My Image](/assets/images/Screen%20Shot%202022-10-04%20at%209.15.16%20AM.png)
+
+When the `Let's Start` button is pressed:
+![My Image](/assets/images/Screen%20Shot%202022-10-04%20at%209.15.47%20AM.png)
+![My Image](/assets/images/Screen%20Shot%202022-10-04%20at%209.16.02%20AM.png)
+
+Winning messages and use of `Hit!` and `Stay`
+![My Image](/assets/images/Screen%20Shot%202022-10-04%20at%201.20.13%20PM.png)
+![My Image](/assets/images/Screen%20Shot%202022-10-04%20at%201.20.44%20PM.png)
+
+## Technologies Used:
+- HTML (Hypertext Markup Language)
+- CSS (Cascading Style Sheets)
+- Javascript
+
+## Getting Started
+
+[Live Site]
+
+WireFrame:
+![My Image](/assets/images/WireFrame-Project-1.jpg)
+
+## Future Enhancements
+
+- Make the background image change once you hit the `Let's Start` button to one of a casino table
+
+- Have a choice in the beginning of the game what kind of deck design you want from a selection
+
+- Add other players than the two player formate to around a maximum of six players
+
+- Add a betting feature along with chips as a visual
+   - You can also at the start of the game decide the pot size
+
+## Pseudo Code
+
+
+
---
+++ scripts/main.js
@@ -0,0 +1,293 @@
+// constant
+const maxScore = 21;
+const points = {
+    'A': 1,
+    '02': 2,
+    '03': 3,
+    '04': 4,
+    '05': 5,
+    '06': 6,
+    '07': 7,
+    '08': 8,
+    '09': 9,
+    '10': 10,
+    'J': 10,
+    'Q': 10,
+    'K': 10,
+}
+
+//state variables
+let gameStart;
+let gameEnd;
+let playerWin;
+let playerCards;
+let dealerCards;
+let playerScore;
+let dealerScore;
+let winningMessage;
+
+// DOM Elements
+let textArea = document.getElementById('text-area');
+let startGame = document.getElementById('start-button');
+let hitButton = document.getElementById('hit-button');
+let stayButton = document.getElementById('stay-button');
+let restartButton = document.getElementById('restart-button')
+let playerContent = document.getElementById('player')
+let dealerContent = document.getElementById('dealer')
+let playerCardsText = document.getElementById('player-cards-text')
+let dealerCardsText = document.getElementById('dealer-cards-text')
+let startGameButton = document.querySelector('#start-button')
+let playerCardsHand = document.getElementById('player-cards')
+let dealerCardsHand = document.getElementById('dealer-cards')
+let playerScoreCount = document.getElementById('player-score')
+let dealerScoreCount = document.getElementById('dealer-score')
+let game = document.querySelector('#game')
+
+// EventListeners
+hitButton.addEventListener('click', playerHitOne);
+startGameButton.addEventListener('click', hidP)
+stayButton.addEventListener('click', stayCards)
+restartButton.addEventListener('click',restart)
+startGameButton.addEventListener('click', ()=>{
+    startGameButton.style.display = 'none';
+    hitButton.style.display = 'block';
+    stayButton.style.display = 'block';
+    restartButton.style.display = 'block';
+    playerContent.style.display ='contents';
+    dealerContent.style.display = 'contents';
+})
+
+class Card{
+    constructor(suit, value){
+        this.suit = suit
+        this.value = value
+    }
+    toString(){
+        return `${this.value} of ${this.suit}`
+    }
+}
+
+class Deck{
+    static suits = ['clubs', 'hearts', 'diamonds', 'spades']
+    static values = ['A', '02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q','K']
+    constructor(){
+        this.deck = []
+        for(let suit of Deck.suits){
+            for(let value of Deck.values){
+                let card = new Card(suit, value)
+                this.deck.push(card)
+            }
+        }
+        this.shuffle()
+    }
+
+    shuffle(){
+          for( let i = 0; i < this.deck.length;i++){
+            let randomIdx = Math.floor(Math.random() * 52)
+            let tempCard = this.deck[i];
+            this.deck[i] = this.deck[randomIdx];
+            this.deck[randomIdx] = tempCard;
+        }
+    }
+
+    deal(number){
+        let drawnHand = [];
+        for(let i = 0; i < number; i ++){
+            drawnHand.push(this.deck.shift())
+        }
+        return drawnHand
+    }
+}
+
+function init(){
+    const dealtDeck = new Deck();
+    playerCards = [];
+    dealerCards = [];
+    dealerCards.push(...dealtDeck.deal(2))
+    playerCards.push(...dealtDeck.deal(2))
+    gameStart = false;
+    gameEnd = false;
+    playerWin = false;
+    playerScore = 0;
+    dealerScore = 0;
+    winningMessage = ''
+    playerContent.style.display = 'none';
+    dealerContent.style.display = 'none';
+    hitButton.style.display = 'none';
+    stayButton.style.display = 'none';
+    restartButton.style.display = 'none';
+}
+
+function hidP(){
+    let el = document.getElementById('hidden');
+        el.style.display = 'none';
+}
+
+function startCardPlayer(){
+       playerCards.forEach(function(card){
+           let cardClass = 'card large'
+           if(['A', 'K', 'Q', 'J'].includes(card.value)){
+               cardClass += ` ${card.suit} ${card.value}`
+           }
+           else{
+               cardClass += ` ${card.suit} r${card.value}`
+           }
+           //create a div element
+           const divEl = document.createElement('div')
+           divEl.className = cardClass
+
+           //append to the DOM
+           playerCardsHand.appendChild(divEl)
+       })
+}
+
+function startCardDealer(){
+    dealerCards.forEach(function(card){
+        let cardClass = 'card large'
+        if(['A', 'K', 'Q', 'J'].includes(card.value)){
+            cardClass += ` ${card.suit} ${card.value}`
+        }
+        else{
+            cardClass += ` ${card.suit} r${card.value}`
+        }
+        const divEl = document.createElement('div')
+        divEl.className = cardClass
+        dealerCardsHand.appendChild(divEl)
+    })
+}
+
+function playerHitOne(){
+    const dealtDeck = new Deck();
+    const hand = dealtDeck.deal(1)
+    playerCards.push(hand[0]);
+     endOfGameCheck();
+    playerAppend(playerCards)
+    playerScoreCount.innerText = playerScore
+}
+
+function dealerHitOne(){
+    const dealtDeck = new Deck();
+    const hand = dealtDeck.deal(1)
+    dealerCards.push(hand[0]);
+    endOfGameCheck();
+    dealerAppend(dealerCards)
+    dealerScoreCount.innerText = dealerScore
+}
+
+function playerAppend(appendingArray){
+    let cardClass = 'card large'
+           if(['A', 'K', 'Q', 'J'].includes(playerCards[playerCards.length - 1].value)){
+               cardClass += ` ${playerCards[playerCards.length - 1].suit} ${playerCards[playerCards.length - 1].value}`
+           }
+           else{
+               cardClass += ` ${playerCards[playerCards.length - 1].suit} r${playerCards[playerCards.length - 1].value}`
+           }
+           const divEl = document.createElement('div')
+           divEl.className = cardClass
+            playerCardsHand.appendChild(divEl)
+}
+
+function dealerAppend(appendingArray){
+    let cardClass = 'card large'
+           if(['A', 'K', 'Q', 'J'].includes(dealerCards[dealerCards.length - 1].value)){
+               cardClass += ` ${dealerCards[dealerCards.length - 1].suit} ${dealerCards[dealerCards.length - 1].value}`
+           }
+           else{
+               cardClass += ` ${dealerCards[dealerCards.length - 1].suit} r${dealerCards[dealerCards.length - 1].value}`
+           }
+           const divEl = document.createElement('div')
+           divEl.className = cardClass
+           dealerCardsHand.appendChild(divEl)
+}
+
+function stayCards(){
+    dealerHitOne();
+    endOfGameCheck();
+};
+
+function removeOldHand(){
+    playerCardsHand.remove()
+    dealerCardsHand.remove()
+}
+
+function scoresOfPlayers(hand){
+    let cardsPoints = 0;
+    let aceCard = false;
+    for( let i = 0; i < hand.length; i++){
+        let card = hand[i];
+        console.log(card)
+        cardsPoints += points[card.value]
+        if(card.value == 'A'){
+            aceCard = true;
+        }
+    }
+    return cardsPoints
+}
+
+function updatedPoints(){
+    playerScore = scoresOfPlayers(playerCards)
+    dealerScore = scoresOfPlayers(dealerCards)
+}
+
+function endOfGameCheck(){
+    updatedPoints()
+
+    if((playerScore === maxScore && dealerScore === maxScore) || (playerScore === dealerScore && playerScore < maxScore )){
+        gameEnd = true;
+        winningMessage = 'It is a Tie!!'
+        hitButton.style.display = 'none';
+        stayButton.style.display = 'none';
+
+    }else if(playerScore > maxScore || dealerScore === maxScore){
+       playerWin = false;
+       gameEnd = true;
+       winningMessage = 'Dealer Wins!!!'
+       hitButton.style.display = 'none';
+       stayButton.style.display = 'none';
+
+    }else if(dealerScore > maxScore || playerScore === maxScore){
+        playerWin = true;
+        gameEnd = true;
+        winningMessage = 'Player Wins!!!'
+        hitButton.style.display = 'none';
+        stayButton.style.display = 'none';
+    }
+    textArea.innerText = winningMessage
+}
+
+function restart(){
+    removeOldHand()
+    const dealtDeck = new Deck();
+    playerCards = [];
+    dealerCards = [];
+    console.log(playerCards)
+    console.log(dealerCards)
+    dealerCards.push(...dealtDeck.deal(2))
+    playerCards.push(...dealtDeck.deal(2))
+    gameStart = false;
+    gameEnd = false;
+    playerWin = false;
+    playerScore = 0;
+    dealerScore = 0;
+    render()
+}
+
+function render(){
+    startCardDealer();
+    startCardPlayer();
+    endOfGameCheck();
+    playerScoreCount.innerText = playerScore
+    dealerScoreCount.innerText = dealerScore
+}
+
+init()
+render()
+
+
+
+
+
+
+
+
+
